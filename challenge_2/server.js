e9e0a9575013c16b943b7921529b346d626332ce
Vincent Castelli <vincentsalvatorecastelli@gmail.com>
2018-05-22 14:20:50 -0700

    Added post functionality.

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -3,15 +3,8 @@ let http = require('http');
 
 let app = express();
 module.exports.app = app;
-// let fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-// module.exports.fullURL = fullUrl;
-let defaultCorsHeaders = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10
-};
-module.exports.headers = defaultCorsHeaders;
+
+app.use(express.static('./client'))
 
 app.get('/', function (req, res) {
   res.send('Here is some data!');
