41c2675a752be0372e1bde00a16e3a540ef0941d
Vincent Castelli <vincentsalvatorecastelli@gmail.com>
2018-05-22 14:12:02 -0700

    Added ajax onClick post request.

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,11 @@
-
-
+$('button').click( function() {
+  $.ajax({
+      url: 'http://127.0.0.1:3000',
+      type: 'post',
+      dataType: 'json',
+      data: $('form').serialize(),
+      success: function(data) {
+                console.log('Sent to get converted!')
+               }
+  });
+});
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,14 +1,26 @@
 let express = require('express');
-
+let http = require('http');
 
 let app = express();
 module.exports.app = app;
+// let fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+// module.exports.fullURL = fullUrl;
+let defaultCorsHeaders = {
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept',
+  'access-control-max-age': 10
+};
+module.exports.headers = defaultCorsHeaders;
 
 app.get('/', function (req, res) {
-  res.send('This is working!');
+  res.send('Here is some data!');
+})
+
+app.post('/', function (req, res) {
+  res.send('Posted to the server!')
 })
 
 app.listen(3000, function () {
   console.log('App server listening on port: 3000');
 })
-
