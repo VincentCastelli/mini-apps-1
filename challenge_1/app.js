87fd43809a4e58c7a2cc1d48af3ee43a9541c550
Vincent Castelli <vincentsalvatorecastelli@gmail.com>
2018-05-21 19:30:55 -0700

    Refactored the createBoard function.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,33 +1,76 @@
+
 // generate a 3x3 game board 
-let createBoard = (n1, n2) => {
-  let gameBoard = [];
-  let gameBoardRow = [];
-  let size = n1 * n2;
-  
-  for (let i = 0; i <= size; i++) {
-    let initValue = 0;
-    if (gameBoardRow.length === 3) {
-      gameBoard.push(gameBoardRow);
-      gameBoardRow = [];
-      gameBoardRow.push(initValue);
-    } else {
-      gameBoardRow.push(initValue);
+let currentPlayer = 0;
+let turn = 0;
+
+let createBoard = () => {
+  let gameBoard = document.getElementById('game-board');
+  let counter = 0;
+
+  for (let i = 0; i < 3; i++) {
+    let row = document.createElement('tr');
+
+    for (let j = 0; j < 3; j++) {
+      let col = document.createElement('td');
+      col.innerHTML = counter;
+
+      row.appendChild(col);
     }
+    gameBoard.appendChild(row);
   }
+  // let gameBoard = [];
+  // let gameBoardRow = [];
+  // let size = n1 * n2;
+  // let html = document.getElementById('game-board');
   
-  return gameBoard;
+  // for (let i = 0; i <= size; i++) {
+  //   let initValue = 0;
+  //   if (gameBoardRow.length === 3) {
+  //     gameBoard.push(gameBoardRow);
+  //     gameBoardRow = [];
+  //     gameBoardRow.push(initValue);
+  //   } else {
+  //     gameBoardRow.push(initValue);
+  //   }
+  // }
+
+  // this.gameBoard.innerHTML = html;
+  // this.cells = this.gameBoard.getElementsByTagName('td')
+
+  // //loop through the cells and add event listener
+  // for (let x = 0; x < this.cells.length; x++) {
+  //   this.cells[x].addEventListener('click', markCell);
+  // }
+
+  // let markCellHandler = (event) => {
+  //   this.markCell(event.target);
+  // }
+
 };
 
-console.log(createBoard(3, 3));
+let winningCombos = new Array();
 
-// render board function
-// let renderBoard = (gameBoard) => {
+let insertCombos = () => {
+    winningCombos.push([1, 2, 3]);
+    winningCombos.push([4, 5, 6]);
+    winningCombos.push([7, 8, 9]);
+    winningCombos.push([1, 4, 7]);
+    winningCombos.push([2, 5, 8]);
+    winningCombos.push([3, 6, 9]);
+    winningCombos.push([1, 5, 9]);
+    winningCombos.push([3, 5, 7]);
+};
 
-// }
+let markCell = () => {
 
-//create an event listener for a click event on a cell
-let handleClick = () => {
-  let element = document.getElementsByClassName('cell');
-
-  element.onClick = () => console.log('I was clicked!');
 }
+
+let checkWinner = () => {
+
+}
+
+let reset = () => {
+
+};
+
+window.onload = createBoard;
