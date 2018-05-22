32ad990d9db7d7f222e0a936ed876a4359e3ae75
Vincent Castelli <vincentsalvatorecastelli@gmail.com>
2018-05-22 12:03:04 -0700

    Created package.json; installed some dependencies; set up server.

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,14 @@
+let express = require('express');
 
+
+let app = express();
+module.exports.app = app;
+
+app.get('/', function (req, res) {
+  res.send('This is working!');
+})
+
+app.listen(3000, function () {
+  console.log('App server listening on port: 3000');
+})
 
