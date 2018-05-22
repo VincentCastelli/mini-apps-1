f559194aecc6d0a29473a34cf54cdf140818caee
Vincent Castelli <vincentsalvatorecastelli@gmail.com>
2018-05-21 17:11:18 -0700

    Create board and initial styling.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,34 @@
+// generate a 3x3 game board 
+let createBoard = (n1, n2) => {
+  let gameBoard = [];
+  let gameBoardRow = [];
+  let size = n1 * n2;
+  
+  for (let i = 0; i <= size; i++) {
+    let initValue = 0;
+    if (gameBoardRow.length === 3) {
+      gameBoard.push(gameBoardRow);
+      gameBoardRow = [];
+      gameBoardRow.push(initValue);
+    } else {
+      gameBoardRow.push(initValue);
+    }
+  }
+  
+  return gameBoard;
+};
 
+console.log(createBoard(3, 3));
 
+// render board function
+// let renderBoard = (gameBoard) => {
+
+// }
+
+//create an event listener for a click event on a cell
+
+// let element = document.getElementsByClassName('cell');
+
+//   element.addEventListner('onClick', function () {
+//     console.log('I was clicked!');
+//   })
