65280dc67346840dc5ae1f2c67775770439e463d
Vincent Castelli <vincentsalvatorecastelli@gmail.com>
2018-05-22 22:04:47 -0700

    Implemented convert helper function.

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,13 +1,14 @@
-$('button').click( function(event) {
-  event.preventDefault();
-  $.ajax({
-      url: 'http://127.0.0.1:3000',
-      type: 'post',
-      dataType: 'json',
-      data: $('form#Submit').serialize(),
-      success: function(data) {
-        console.log('Sent to get converted!')
-      }
-
+let sendData = (data) => {
+  $('#submit').on('click', (event) => {
+    event.preventDefault();
+    $.ajax({
+        url: 'http://127.0.0.1:3000',
+        type: 'post',
+        contentType: 'application/json',
+        data: data,
+        success: (data) => {
+          $('#result').append(data);
+        }
+    });
   });
-});
+};
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -7,20 +7,49 @@ let fs = require('fs');
 let app = express();
 module.exports.app = app;
 
-app.use(bodyParser.json());  
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static('./client'))
 
 
 app.get('/', function (req, res) {
-  res.send('GET request')
+  res.status(200).send('/client/index.html');
 })
 
 app.post('/', function (req, res) {
   // convert JSON to CSV and save and send it back
-  res.send(req.body);
+  // res.send('POST Request');
+  let csv = (convert(req.body));
+  console.log(csv);
+  res.status(200).send(csv);
 })
 
 app.listen(3000, function () {
   console.log('App server listening on port: 3000');
 })
+
+let convert = (obj) => {
+  // keys not equal to children ["firstName", "lastName", "county", "city", "role", "sales"]
+  let csvArray = [];
+  let key = Object.keys(obj).filter( key => key  !== 'children').join(',') + '\n';
+    csvArray.push(key);
+    //inner recursion; 
+    let innerRecurse = function(obj) {
+      let tempArray = [];
+      for (let key in obj) {
+        if (key !== 'children') {
+          // push obj[key[i]] to the array
+          tempArray.push(obj[key]);
+        }
+      }
+      // append a new line after each pass through an obj 
+      csvArray.push(tempArray.join() + '\n');
+      // if there are children, invoke recursion on child
+      if (obj.children) {
+        obj.children.forEach((child) => innerRecurse(child));
+      }
+    };
+    // call innerRecurse
+    innerRecurse(obj);
+  
+    return csvArray.join('');
+}
