be1e36039690f3a4a7ef8a751d9ce3491226d262
Vincent Castelli <vincentsalvatorecastelli@gmail.com>
2018-05-23 11:16:52 -0700

    Environment setup; compiled files; server online.

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,17 @@
+const express = require('express');
 
+const app = express();
 
+app.use(express.static('/public'));
+
+app.get('/', (req, res) => {
+  res.send('This is working!');
+});
+
+app.post('/', (req, res) => {
+  res.send('Post sent!');
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000:');
+});
